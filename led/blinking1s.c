cfcddd1e18a38cae8ce7eefda42fe88df1fabf18
mir <alan4chen@kimo.com>
2016-11-23 15:55:52 +0800

    [RASP] blinking

--- led/blinking1s.c
+++ led/blinking1s.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+#include <unistd.h>
 int main(){
 	while(1){
 		FILE *f = fopen("/sys/class/leds/led0/shot", "w");
 		if (f == NULL)
 		{
     		printf("Error opening file!\n");
-    		exit(1);
+    		//exit(1);
 		}
 		const char *text = "1";
 		fprintf(f, "%s", text);
 		fclose(f);
+    usleep(1000000);
 	}
 }
