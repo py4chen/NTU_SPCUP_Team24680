1c7515806acb55200d45b40fc6043c77bcdbc71c
alan <alan4chen@kimo.com>
2016-11-23 15:46:42 +0800

    [LED]

--- led/blinking1s.c
+++ led/blinking1s.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include <Windows.h>
 
 int main(){
 	while(1){
-		printf("hello\n");
-		Sleep(1000000);
+		FILE *f = fopen("/sys/class/leds/led0/shot", "w");
+		if (f == NULL)
+		{
+    		printf("Error opening file!\n");
+    		exit(1);
+		}
+		const char *text = "1";
+		fprintf(f, "%s", text);
+		fclose(f);
 	}
 }
