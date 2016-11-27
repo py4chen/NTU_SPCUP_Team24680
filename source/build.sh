ba30929a06a3cc1f73a5d4e171cb33d48173a819
hyww13 <willy2202@gmail.com>
2016-11-27 14:47:14 +0800

    build.sh: gcc <input file> goes first

--- source/build.sh
+++ source/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 file="recorder"
 aubio="../../aubio" # path of aubio
-gcc -Wall -l asound -L $aubio/build/src -l aubio -I $aubio/src $file.c -o $file
+gcc $file.c -Wall -l asound -L $aubio/build/src -l aubio -I $aubio/src -o $file
