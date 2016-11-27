1087176d13ed158df5b93ddf8d0ce99d0d53ff56
hyww13 <willy2202@gmail.com>
2016-11-27 13:24:16 +0800

    build.sh: remove useless gcc flags

--- source/build.sh
+++ source/build.sh
@@ -2,4 +2,4 @@
 file="recorder"
 aubio="../../aubio" # path of aubio
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$aubio/build/src
-gcc -Wall -l asound -L $aubio/build/src -l aubio -I $aubio/build/src/ -I $aubio/tests/ -I $aubio/src -lm $file.c -o $file
+gcc -Wall -l asound -L $aubio/build/src -l aubio -I $aubio/src $file.c -o $file
