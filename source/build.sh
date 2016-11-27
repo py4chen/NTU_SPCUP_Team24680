bc558c90d47ebe36ca41b29ec91798642e6da889
hyww13 <willy2202@gmail.com>
2016-11-27 13:55:39 +0800

    build.sh: don't set LD_LIBRARY_PATH here

--- source/build.sh
+++ source/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 file="recorder"
 aubio="../../aubio" # path of aubio
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$aubio/build/src
 gcc -Wall -l asound -L $aubio/build/src -l aubio -I $aubio/src $file.c -o $file
