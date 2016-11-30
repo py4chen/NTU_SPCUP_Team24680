df8f7f400e79f98c2bb15e05c31e6affda6e15cc
alan <alan4chen@kimo.com>
2016-11-30 17:12:48 +0800

    flow print

--- source/flow.c
+++ source/flow.c
@@ -50,6 +50,8 @@ static void sigHandler(int sig){
 		}
 		remain.tv_sec = (next_time-cur_time)/1000000;
 		remain.tv_nsec = (next_time-cur_time - remain.tv_sec) * 1000;
+		printf("remain: %2ld.%09ld\n", (long)remain.tv_sec,
+        			remain.tv_nsec);
     }
 	return;
 }
