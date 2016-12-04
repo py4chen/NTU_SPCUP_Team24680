63b3fc4dd603b1a95573cb2cf25c623e57be0345
mir <alan4chen@kimo.com>
2016-12-04 15:17:44 +0800

    rasp debug

--- source/led.c
+++ source/led.c
@@ -7,18 +7,19 @@
 #include "structure.h"
 #endif
 
-void _ledACT(){
+void ledACT(){
 	FILE *f = fopen("/sys/class/leds/led0/shot", "w");
 	if (f == NULL)
 	{
     	printf("Error opening file!\n");
+      exit(-1);
 	}
 	const char *text = "1";
 	fprintf(f, "%s", text);
 	fclose(f);
 }
 
-void ledACT(Message *addr){
+void _ledACT(Message *addr){
 	FILE *fp = fopen("./text.txt", "aw");
 	fprintf(fp ,"LED0! Current Time:%llu  Relative Time:%llu \n\n", 
 		getCurrentTimestamp(), getCurrentTimestamp()-addr->start_time);
